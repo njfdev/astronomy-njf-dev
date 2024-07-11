8d6e908ac761c5cf0e95240375911bf4f873db20
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-11 18:29:59 -0400

    Add Noninteractive Date Range Field

--- components/PhotoGrid.tsx
+++ components/PhotoGrid.tsx
@@ -1,4 +1,5 @@
 import PhotoCard from "@/components/PhotoCard";
+import { DateRangePicker, Select, SelectItem } from "@nextui-org/react";
 
 export default function PhotoGrid({
   children,
@@ -8,16 +9,23 @@ export default function PhotoGrid({
   pictureData: { folder: string; pageUrl?: string }[];
 }) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {pictureData.map((data) => (
-        <PhotoCard
-          key={data.folder}
-          pictureFolder={data.folder}
-          pageUrl={data.pageUrl}
-        >
-          {children}
-        </PhotoCard>
-      ))}
-    </div>
+    <>
+      <div className="w-full">
+        <div className="flex flex-col">
+          <DateRangePicker label="Photo Date Range" />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {pictureData.map((data) => (
+          <PhotoCard
+            key={data.folder}
+            pictureFolder={data.folder}
+            pageUrl={data.pageUrl}
+          >
+            {children}
+          </PhotoCard>
+        ))}
+      </div>
+    </>
   );
 }

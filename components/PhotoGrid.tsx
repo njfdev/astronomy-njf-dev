6035426afa96b4ca332b19ce67cba3c4443dccb7
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-12 11:17:05 -0400

    Add Object Type and Sort Filter UI

--- components/PhotoGrid.tsx
+++ components/PhotoGrid.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import PhotoCard from "@/components/PhotoCard";
 import { DateRangePicker, Select, SelectItem } from "@nextui-org/react";
+
+// TODO: remove this temporary variable
+const objectTypes: string[] = [
+  "Galaxy",
+  "Nebula",
+  "Star Cluster",
+  "Lunar",
+  "Solar",
+  "Eclipse",
+];
+
+const sortOptions: string[] = ["Old -> New", "New -> Old", "A -> Z", "Z -> A"];
 
 export default function PhotoGrid({
   children,
@@ -10,10 +24,18 @@ export default function PhotoGrid({
 }) {
   return (
     <>
-      <div className="w-full">
-        <div className="flex flex-col">
-          <DateRangePicker label="Photo Date Range" />
-        </div>
+      <div className="w-full flex">
+        <Select label="Sort">
+          {sortOptions.map((sortOption: string) => (
+            <SelectItem key={sortOption}>{sortOption}</SelectItem>
+          ))}
+        </Select>
+        <Select label="Object Type" selectionMode="multiple">
+          {objectTypes.map((objectType: string) => (
+            <SelectItem key={objectType}>{objectType}</SelectItem>
+          ))}
+        </Select>
+        <DateRangePicker label="Photo Date Range" />
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {pictureData.map((data) => (

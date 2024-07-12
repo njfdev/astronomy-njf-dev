4ffd239563d55a04ad217ffe825fa100adb9d081
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-12 16:03:11 -0400

    Add Umami Analytics Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} prose dark:prose-invert max-w-none`}>
+        <Script
+          defer={true}
+          src="https://umami.njf.dev/script.js"
+          data-website-id="26321b22-2c40-4dd8-98b5-a56369df6a0b"
+        ></Script>
         <Providers>
           <main className="p-4 max-w-none">{children}</main>
         </Providers>

5e8a1e0bd7f81ea42912b01f1704d88bbdbe2e74
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-13 12:55:06 -0400

    refactor: move getPhotoDetails function into utils folder

--- components/PhotoGrid.tsx
+++ components/PhotoGrid.tsx
@@ -21,6 +21,7 @@ import { PhotoDetails } from "@/types/metadata";
 import { objectInfo } from "@/utils/objectInfo";
 import { parseDate } from "@internationalized/date";
 import { FaFilterCircleXmark } from "react-icons/fa6";
+import getPhotoDetails from "@/utils/photos";
 
 interface PictureData {
   folder: string;
@@ -224,55 +225,6 @@ export default function PhotoGrid({
       </div>
     </>
   );
-}
-
-function getPhotoDetails(pictureFolder: string): PhotoDetails {
-  let timeIsSpecified = false;
-
-  const objectName = pictureFolder.split("/")[0];
-  const folderDate = moment
-    .tz(
-      pictureFolder.split("/")[1].split("_")[0].split(" ")[0],
-      "America/New_York"
-    )
-    .toDate();
-
-  const photoData = require(`../public/astrophotos/${pictureFolder}/${objectName}\ -\ Final.json`);
-
-  let pictureDate = folderDate;
-
-  if (photoData["DATE-OBS"]) {
-    const dateObs = removeOuterQuotes(photoData["DATE-OBS"]?.[0]);
-    const dateObsUTC = new Date(dateObs + "Z");
-    if (
-      dateObsUTC.toLocaleDateString("en-US", {
-        timeZone: "America/New_York",
-      }) ===
-      folderDate.toLocaleDateString("en-US", {
-        timeZone: "America/New_York",
-      })
-    ) {
-      pictureDate = new Date(dateObs + "Z");
-      timeIsSpecified = true;
-    }
-  }
-
-  const telescopeName =
-    removeOuterQuotes(photoData["TELESCOP"]?.[0]) || "Seestar S50";
-
-  const exposureTime = removeOuterQuotes(photoData["LIVETIME"]?.[0]);
-
-  let objectDetails = objectInfo[objectName as keyof typeof objectInfo];
-
-  return {
-    timeIsSpecified,
-    telescopeName,
-    exposureTime,
-    objectName,
-    pictureFolder,
-    pictureDate,
-    objectDetails,
-  };
 }
 
 function getDateDifference(a: PictureData, b: PictureData) {
